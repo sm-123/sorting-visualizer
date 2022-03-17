3ea9b8d9889a9739d3ca20541044e6b865e9a3e7
sm-123 <sourav16021998@gmail.com>
2022-03-17 15:55:19 -0700

    JS

--- js_files/sorting.js
+++ js_files/sorting.js
@@ -3,3 +3,12 @@ const randomArray = []
 for(let i = 0; i<ARRAY_LENGTH; i++) {
     randomArray.push(Math.random())
 }
+
+var div = document.createElement("div");
+div.style.width = "100px";
+div.style.height = "100px";
+div.style.background = "red";
+
+
+document.getElementById("bars").appendChild(div);
+
